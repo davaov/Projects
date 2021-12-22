3c4e5e497c4e38687d1a784722a44d0cd954df3e
davaov <96546268+davaov@users.noreply.github.com>
2021-12-22 23:38:50 +0300

    ticketSystem

---
+++ ticketSystem/connectionStaticStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticketSystem
+{
+    class connectionStaticStrings
+    {
+        //public static string connectionstr = @"Data Source=DESKTOP-793DC9A\SQLEXPRESS; Initial Catalog = ticketSystem; Integrated Security = True";
+        public static string connectionstr = @"Server=DESKTOP-793DC9A\SQLEXPRESS;Database=master;Trusted_Connection=True;";
+    }
+}
