55f2529ca5fe0de9696bb960a2dde59ad4bc22b4
davaov <96546268+davaov@users.noreply.github.com>
2022-01-12 23:52:12 +0300

    gf

---
+++ ticketSystem/ticketSystem/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticketSystem
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection();
+        SqlDataReader dataReader;
+        SqlCommand command;
+        Admin adm = new Admin();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.ConnectionString = connectionStaticStrings.connectionstr;
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Связь с сервером установлена");
+            }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 17: MessageBox.Show("Ошибка", "Неверное имя сервера!", MessageBoxButtons.OK, MessageBoxIcon.Error); break;
+                    case 4060: MessageBox.Show("Ошибка", "Неверное имя БД!", MessageBoxButtons.OK, MessageBoxIcon.Error); break;
+                    case 18456: MessageBox.Show("Ошибка", "Неверное имя пользователя или пароль!", MessageBoxButtons.OK, MessageBoxIcon.Error); break;
+                }
+                MessageBox.Show(ex.Message + " Уровень ошибки " + ex.Class); return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения " + ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ ticketSystem/ticketSystem/connectionStaticStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ticketSystem
+{
+    class connectionStaticStrings
+    {
+        public static string connectionstr = @"Data Source=DESKTOP-793DC9A; Initial Catalog = ticketSystem; Integrated Security = True";
+    }
+}
